f0fab44a3c06e3beaabe794be5f47dddef24cfc0
ky_blurblue <kaiya_job@yahoo.co.jp>
2022-05-31 01:43:07 +0900

    星をクリックするとレートを変更する機能を追加。続きはP123の6.3から

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ import ColorList from './ColorList';
 
 export default function App() {
   const [colors, setColors] = useState(colorData);
+
+  const onRateColor = (id, rating) => {
+    const newColors = colors.map(color =>
+      color.id === id ? {...color, rating} : color);
+      setColors(newColors);
+  };
+
   return <ColorList colors={colors} onRemoveColor={id => {
     const newColors = colors.filter(color => color.id !== id);
     setColors(newColors);
   }}
+  onRateColor={onRateColor}
   />;
 }
