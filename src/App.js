f1de333e0bd80a5988f11f06af823cbae3a41272
ky_blurblue <kaiya_job@yahoo.co.jp>
2022-06-02 23:24:41 +0900

    colorListに新しい要素を追加するAddColorFromを実装。続きはP131の6.4から

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import colorData from "./color-data.json";
 // import StarRating from './StarRating';
 import ColorList from './ColorList';
+import { v4 } from "uuid";
+import AddColorForm from './AddColorForm';
 
 export default function App() {
   const [colors, setColors] = useState(colorData);
@@ -13,10 +15,25 @@ export default function App() {
       setColors(newColors);
   };
 
-  return <ColorList colors={colors} onRemoveColor={id => {
-    const newColors = colors.filter(color => color.id !== id);
-    setColors(newColors);
-  }}
-  onRateColor={onRateColor}
-  />;
+  return (
+    <>
+      <AddColorForm onNewColor={(title, color)=> {
+        const newColors = [
+          ...colors,
+          {
+            id: v4(),
+            rating: 0,
+            title,
+            color
+          }
+        ];
+        setColors(newColors);
+      }} />
+      <ColorList colors={colors} onRemoveColor={id => {
+      const newColors = colors.filter(color => color.id !== id);
+      setColors(newColors);
+    }}
+    onRateColor={onRateColor} />
+  </>
+  );
 }
